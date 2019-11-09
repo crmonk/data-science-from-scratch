07a3df4bc75e709c3f4f3009ac88682307f8e386
Chris Monk <crmonk35@gmail.com>
2019-11-09 19:12:14 +0000

    Up to start of 'Paid Accounts' in Introudctions completed

--- scratch/introduction.py
+++ scratch/introduction.py
@@ -99,8 +99,7 @@ def friends_of_friends(user):
 
 
 #print(friends_of_friends(users[3]))               # Counter({0: 2, 5: 1})
-
-
+    
 assert friends_of_friends(users[3]) == Counter({0: 2, 5: 1})
 
 interests = [
--- scripts/intro_crm.py
+++ scripts/intro_crm.py
@@ -1,4 +1,4 @@
-from scratch.introduction import users, friendship_pairs, interests
+from scratch.introduction import users, friendship_pairs, interests, salaries_and_tenures
 
 
 friendships = {user["id"]: [] for user in users}
@@ -62,8 +62,71 @@ def friends_of_friends(user):
 def data_scientists_who_like(target_interest):
 	""" Find the ids of all users who like the target interest. """
 	return[user_id 
-			for user_id, interest_name in interests 
-			if interest_name == target_interest
+			for user_id, interest in interests 
+			if interest == target_interest
 	]
+#print(data_scientists_who_like('Hadoop')) ~#[0,9]
 
-print(data_scientists_who_like('Hadoop'))
+from collections import defaultdict
+
+# Keys are interests, values are lists of user_ids with that interest
+user_ids_by_interest = defaultdict(list)
+for user_id, interest in interests:
+		user_ids_by_interest[interest].append(user_id)
+
+interests_by_user_id = defaultdict(list)
+for user_id, interest in interests:
+	interests_by_user_id[user_id].append(interest)
+
+"""
+iterate over user's interests
+lookup list of users interested in that from index
+"""
+
+def most_common_interests_with(user):
+	return Counter(
+		interested_user_id
+		for interest in interests_by_user_id[user["id"]]			# for each of my interests
+		for interested_user_id in user_ids_by_interest[interest]	# find other users also interested
+		if interested_user_id != user["id"]							# except for me
+	)
+
+assert(most_common_interests_with(users[0]) == Counter({9: 3, 1: 2, 8: 1, 5: 1}))
+
+#---------------------------------------------------------------------------------------------------------
+
+""" Keys are years, values are lists of the salaries for each tenure """
+salary_by_tenure = defaultdict(list)
+
+for salary, tenure in salaries_and_tenures:
+	salary_by_tenure[tenure].append(salary)
+
+""" Keys are years,each value is average salary for that tenure """
+average_salary_by_tenure = {
+	tenure: sum(salaries)/ len(salaries)
+	for tenure, salaries in salary_by_tenure.items()
+}
+
+def tenure_bucket(tenure):
+	if tenure < 2:
+		return "less than 2"
+	elif tenure < 5:
+		return "between 2 and 5"
+	else:
+		return "over 5"
+
+salaries_by_tenure_bucket = defaultdict(list)
+
+for salary,tenure in salaries_and_tenures:
+	bucket = tenure_bucket(tenure)
+	salaries_by_tenure_bucket[bucket].append(salary)
+
+""" Keys are tenure buckets,each value is average salary for that tenure bucket """
+average_salary_by_tenure = {
+	tenure_bucket: sum(salaries)/ len(salaries)
+	for tenure_bucket, salaries in salaries_by_tenure_bucket.items()
+}
+
+print(salaries_by_tenure_bucket)
+
+print(average_salary_by_tenure)
